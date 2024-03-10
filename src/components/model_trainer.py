f06c1fd5376000879ceca6b11ed58dbd6d3f7d81
Ryan <ryandcondiman@gmail.com>
2024-03-10 16:06:33 +0800

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,7 +48,40 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            model_report:dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            param_grid={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2']
+                },
+                "Random Forest":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_features':['sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'subsample':[0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'max_features':['sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]                
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+            model_report:dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param_grid=param_grid)
             print(model_report)
             print('\n====================================================================================\n')
             logging.info(f"Model Report: {model_report}")
@@ -68,8 +101,9 @@ class ModelTrainer:
             print('\n====================================================================================\n')
             logging.info(f"Best Model Found, Model Name: {best_model_name}, R2 Score: {best_model_score}")
 
+            logging.info("Saving the best model...")
             save_object(file_path=self.model_trainer_config.trained_model_file_path, obj=best_model)
-
+            logging.info("Best model saved.")
             predicted = best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ from src.exception import CustomException
 from src.logger import logging
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -21,15 +22,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, param_grid=None):
     try:
         report = {}
 
         for model_name, model in models.items():
-            model.fit(X_train, y_train)
+            if param_grid is not None and model_name in param_grid:
+                grid_search = GridSearchCV(model, param_grid[model_name], scoring='r2', cv=3, n_jobs=-1)
+                grid_search.fit(X_train, y_train)
+                best_model = grid_search.best_estimator_
+            else:
+                best_model = model
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            best_model.fit(X_train, y_train)
+
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
