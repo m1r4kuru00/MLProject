f407fe07cf9f49f30d560888e49893ae5d1ab3f3
Ryan <ryandcondiman@gmail.com>
2024-03-10 15:45:35 +0800

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,9 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 
 @dataclass
@@ -49,7 +52,15 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data,test_data = obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    
+    try:
+        result = model_trainer.initiate_model_trainer(train_arr, test_arr)
+        print("Model training successful. R2 Score:", result)
+    except Exception as e:
+        print("Error during model training:", str(e))
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -110,5 +110,3 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e,sys)
         
-        
-
